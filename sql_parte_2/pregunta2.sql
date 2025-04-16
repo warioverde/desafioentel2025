b190958b961d513e047ba1bf168cbc8229ca8fec
warioverde <tatan.alegria@gmail.com>
2025-04-16 13:47:34 -0400

    se agregar order by por nro de empleados pregunta2.sql

--- sql_parte_2/pregunta2.sql
+++ sql_parte_2/pregunta2.sql
@@ -4,4 +4,4 @@ select * from raw_hired_employees where department_id is not null and SPLIT_PART
 ) group by department_id) inner join raw_departments on raw_departments.id = department_id where hired_employess > (select avg(hired_employess) from (
 select department_id, count(*) as hired_employess from (
 select * from raw_hired_employees where department_id is not null and SPLIT_PART(datetime, '-', 1) = '2021'
-) group by department_id));
+) group by department_id)) order by hired_employess DESC;
